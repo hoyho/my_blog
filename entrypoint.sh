3057246e5e4efac6227cbe9602a65162d7ceb527
ubuntu <luohaihao@gmail.com>
2019-06-18 22:19:44 +0800

    update docker config

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 python manage.py migrate                  # Apply database migrations
 python manage.py collectstatic --noinput  # Collect static file
 
-uwsgi --ini /app/my_blog/webconfig_uwsgi.ini &
+uwsgi --ini /app/my_blog/webconfig_uwsgi_docker.ini &
 
 #EXPOSE 80
 #CMD ["supervisord", "-n"]
